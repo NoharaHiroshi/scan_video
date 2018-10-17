92dd4e7350858049db708ab1fdb6630e498cfa78
Lands <380788433@qq.com>
2018-10-17 18:40:51 +0800

    视频下载进度条

--- scan_video.py
+++ scan_video.py
@@ -2,28 +2,30 @@
 
 import requests
 import urllib
+import urllib2
 import time
 import os
 import ssl
 import math
+import sys
 
 
-def calc_download_speed(block_num, block_size, total_size, start_time):
-    """
-    :param block_num: 已下载的数据块大小
-    :param block_size: 数据块大小
-    :param total_size: 文件总大小
-    :param start_time: 开始下载时的时间点
-    :return:
-    """
-    # 已下载百分比
-    download_percent = '%.2f' % (block_num * block_size * 100 / total_size)
-    speed_bytes = (block_num * block_size) / (time.time() - start_time)
-    if speed_bytes < 1024:
-        speed_str = '%s KB/s' % float(speed_bytes / 1024)
-    else:
-        speed_str = '%s MB/s' % float(speed_bytes / 1024)
-    print '当前下载速度：%s | 当前下载百分比：%s' % (speed_str, download_percent)
+def download_bar(total_size, current_size, block_size, start_time, end_time):
+    download_flag = u'|'
+    not_download_flag = u'·'
+    percent = int(float(current_size) / total_size * 20)
+    current_speed = float(block_size) / (end_time - start_time)
+    percent_bar_str = r'[ %s%s ] - %s%% | total_size: %s ' \
+                      '| download_size: %s | speed: %s / s' % (download_flag * percent,
+                                                               not_download_flag * (20 - percent),
+                                                               percent * 5,
+                                                               convert_storage_read(total_size),
+                                                               convert_storage_read(current_size),
+                                                               convert_storage_read(current_speed)
+                                                               )
+    print percent_bar_str
+    sys.stdout.flush()
+    time.sleep(1)
 
 
 def convert_storage_read(bytes_content):
@@ -41,7 +43,7 @@ def convert_storage_read(bytes_content):
 
 def scan_video():
     headers = {
-        'Host': 'upos-hz-mirrorcos.acgvideo.com',
+        'Host': 'upos-hz-mirrorkodo.acgvideo.com',
         'Connection': 'keep-alive',
         'Origin': 'https://www.bilibili.com',
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36',
@@ -55,7 +57,7 @@ def scan_video():
     # 网络中数据流的初始化大小
     all_content_length = 0
     # 请求的url
-    url = r'https://upos-hz-mirrorkodo.acgvideo.com/upgcxcode/07/82/50108207/50108207-1-32.flv?e=ig8euxZM2rNcNbNVnWRVhoMMhW4ghwdEto8g5X10ugNcXBlqNxHxNEVE5XREto8KqJZHUa6m5J0SqE85tZvEuENvNC8xNEVE9EKE9IMvXBvE2ENvNCImNEVEK9GVqJIwqa80WXIekXRE9IB5QK==&deadline=1539680709&dynamic=1&gen=playurl&oi=1897879350&os=kodo&platform=pc&rate=209100&trid=a3ca314910e04caabee6c9bbdfb2819c&uipk=5&uipv=5&um_deadline=1539680709&um_sign=9d7cf242ca446644c7766b3ab775c5dd&upsig=711f29a36422d453797dce8ba73078d1'
+    url = r'https://upos-hz-mirrorkodo.acgvideo.com/upgcxcode/07/82/50108207/50108207-1-32.flv?e=ig8euxZM2rNcNbNVnWRVhoMMhW4ghwdEto8g5X10ugNcXBlqNxHxNEVE5XREto8KqJZHUa6m5J0SqE85tZvEuENvNC8xNEVE9EKE9IMvXBvE2ENvNCImNEVEK9GVqJIwqa80WXIekXRE9IB5QK==&deadline=1539774028&dynamic=1&gen=playurl&oi=1897879350&os=kodo&platform=pc&rate=209100&trid=1fa10cec170741c4aab5a35dbb14975b&uipk=5&uipv=5&um_deadline=1539774028&um_sign=751a892345e4dffcd9fe6d3057f5671d&upsig=1dcd7beb98d7ead2d9f679b8c870df7f'
     file_path = os.path.join(os.path.dirname(__file__), '1.flv')
     try:
         if not all_content_length:
@@ -75,11 +77,21 @@ def scan_video():
 
             with open(file_path, 'ab') as f:
                 # 在此处下载数据
-                f.write(response.content)
-                # 从内存中写入文件
-                f.flush()
-                print('receive data，file size : %s   total size: %s' %
-                      (convert_storage_read(os.path.getsize(file_path)), convert_storage_read(all_content_length)))
+                current_length = os.path.getsize(file_path)
+                # 块大小1m, 1m 刷新一次
+                block_size = pow(1024, 2)
+                start_time = time.time()
+                for block in response.iter_content(chunk_size=block_size):
+                    if block:
+                        f.write(block)
+                        f.flush()
+                        current_length += block_size
+                        if current_length > all_content_length:
+                            current_length = all_content_length
+                        end_time = time.time()
+                        print all_content_length, current_length
+                        download_bar(all_content_length, current_length, block_size, start_time, end_time)
+                        start_time = time.time()
     except Exception as e:
         print e
 
